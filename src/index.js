a27b54af9b32d5e374325191b0f44cafec77fafb
Android Studio <devandroid@ort>
2025-04-11 12:11:58 -0300

    tp02

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log("hola")
+
+import express from "express"; // hacer npm i express
+import cors from "cors"; // hacer npm i cors
+const app = express();
+const port = 3000; // El puerto 3000 (http://localhost:3000)
+// Agrego los Middlewares
+app.use(cors()); // Middleware de CORS
+app.use(express.json()); // Middleware para parsear y comprender JSON
+//
+// Aca pongo todos los EndPoints
+//
+app.get('/', (req, res) => { // EndPoint "/"
+res.send('Ya estoy respondiendo!');
+})
+
+app.get('/saludar/:nombre', (req, res) => { // EndPoint "/saludar"
+let nombre=req.params.nombre
+res.status(200).send(`Hola ${nombre}`);
+})
+
+app.get("/validarfecha/:ano/:", (req, res) => { // EndPoint "/saludar"
+    let mes=req.params.mes
+    let ano=req.params.ano
+    let dia=req.params.dia
+
+    fecha=Date.parse;
+
+    if(fecha=isNaN)
+    {
+        res.status(400).send(`BAD REQUEST`);
+    }
+    else{
+        res.status(200).send(`OK`);
+    }
+    
+    })
+//
+// Inicio el Server y lo pongo a escuchar.
+//
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`)
+})
