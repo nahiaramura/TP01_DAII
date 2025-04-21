ea9a54b87e97e5f0203c75ec77640ef8fda13ddf
Android Studio <devandroid@ort>
2025-04-21 08:55:44 -0300

    A Y B hechos

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ app.use(express.json()); // Middleware para parsear y comprender JSON
 //
 // Aca pongo todos los EndPoints
 //
+
+//A
 app.get('/', (req, res) => { // EndPoint "/"
 res.send('Ya estoy respondiendo!');
 })
@@ -18,21 +20,78 @@ let nombre=req.params.nombre
 res.status(200).send(`Hola ${nombre}`);
 })
 
-app.get("/validarfecha/:ano/:", (req, res) => { // EndPoint "/saludar"
+app.get("/validarfecha/:ano/:mes/:dia", (req, res) => { 
     let mes=req.params.mes
     let ano=req.params.ano
     let dia=req.params.dia
 
-    fecha=Date.parse;
+    let fechaStr = `${ano}-${mes}-${dia}`;
+    let fecha = Date.parse(fechaStr);
 
-    if(fecha=isNaN)
-    {
-        res.status(400).send(`BAD REQUEST`);
-    }
-    else{
-        res.status(200).send(`OK`);
+    if (isNaN(fecha)) {
+        res.status(400).send("BAD REQUEST");
+    } else {
+        res.status(200).send("OK");
     }
     
+    })
+
+    //B
+    app.get("/matematica/sumar", (req, res) => {
+        const n1 = parseFloat(req.query.n1);
+        const n2 = parseFloat(req.query.n2);
+      
+        if (isNaN(n1) || isNaN(n2)) {
+          return res.status(400).send("Parámetros inválidos");
+        }
+      
+        const resultado = sumar(n1, n2);
+        res.status(200).send(`Resultado: ${resultado}`);
+      });
+
+      
+    app.get("/matematica/restar", (req, res) => {
+        const n1 = parseFloat(req.query.n1);
+        const n2 = parseFloat(req.query.n2);
+      
+        if (isNaN(n1) || isNaN(n2)) {
+          return res.status(400).send("Parámetros inválidos");
+        }
+      
+        const resultado = restar(n1, n2);
+        res.status(200).send(`Resultado: ${resultado}`);
+      });
+
+      app.get("/matematica/multiplicar", (req, res) => {
+        const n1 = parseFloat(req.query.n1);
+        const n2 = parseFloat(req.query.n2);
+      
+        if (isNaN(n1) || isNaN(n2)) {
+          return res.status(400).send("Parámetros inválidos");
+        }
+      
+        const resultado = multiplicar(n1, n2);
+        res.status(200).send(`Resultado: ${resultado}`);
+      });
+
+
+      app.get("/matematica/dividir", (req, res) => {
+        const n1 = parseFloat(req.query.n1);
+        const n2 = parseFloat(req.query.n2);
+      
+        if (isNaN(n1) || isNaN(n2)) {
+          return res.status(400).send("Parámetros inválidos");
+        }
+      
+        const resultado = dividir(n1, n2);
+        res.status(200).send(`Resultado: ${resultado}`);
+      });
+
+//C
+app.get('/omdb/searchbypage', async (req, res) => {
+ 
+    let resultado = await metodoAsincronico();
+
     })
 //
 // Inicio el Server y lo pongo a escuchar.
